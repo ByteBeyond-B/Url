c9f033a4fd36d5dfe663ada58bf629aa4297495d
Aarya Sharma <sharma1sites@gmail.com>
2025-02-24 09:31:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,29 @@ self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open("quicklink-cache").then((cache) => {
             return cache.addAll([
-                "/",
-                "/index.html",
-                "/manifest.json",
-                "/QuickLink_App_Icon.png",  // Logo
-                "/QuickLink_Splash_Screen_1500x2688.png"  // Splash Screen
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "https://ik.imagekit.io/rsmeguisr/QuickLink_App_Icon.png",
+                "https://ik.imagekit.io/rsmeguisr/QuickLink_Splash_Screen_1500x2688.png"
             ]);
         })
     );
+    self.skipWaiting();
     console.log("Service Worker Installed");
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+    console.log("Service Worker Activated");
 });
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
+        }).catch(() => {
+            return caches.match("./index.html");
         })
     );
 });
